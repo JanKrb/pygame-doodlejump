e81e37b6b212eb19353a3535efa2d6ff7790c20f
Jan Ruhfus <janruhfus0@gmail.com>
2022-02-25 10:37:28 +0100

    Add basic pygame structure with background

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pygame
 
 class Path:
     runtime_path = os.path.dirname(os.path.realpath(__file__))
@@ -38,4 +39,49 @@ class Config:
         with open(self.config_path, 'w') as config_file:
             json.dump(config, config_file)
 
-config = Config()
+class Background:
+    def __init__(self, config: Config) -> None:
+        self.config = config
+        self.background = pygame.image.load(os.path.join(Path.assets_images_path, config.config['images']['background'])).convert()
+        self.background = pygame.transform.scale(self.background, (config.config['screen']['width'], config.config['screen']['height']))
+    
+    def draw(self, screen: pygame.Surface) -> None:
+        screen.blit(self.background, (0, 0))
+    
+    def update(self, config: Config) -> None:
+        pass
+
+class Game:
+    def __init__(self, config: Config) -> None:
+        self.config = config
+        self.screen = pygame.display.set_mode((self.config.config['screen']['width'], self.config.config['screen']['height']))
+        pygame.display.set_caption(self.config.config['screen']['title'])
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        self.background = Background(config)
+    
+    def run(self) -> None:
+        while self.running:
+            self.clock.tick(self.config.config['screen']['fps'])
+            self.events()
+            self.update()
+            self.draw()
+    
+    def events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+    
+    def update(self) -> None:
+        self.background.update(self.config)
+    
+    def draw(self) -> None:
+        self.background.draw(self.screen)
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    config = Config()
+    game = Game(config)
+    game.run()
